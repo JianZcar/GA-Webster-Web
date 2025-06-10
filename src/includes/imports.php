9f758a6a232fb96ef760c81478e8b13512635e5d
JianZcar <esteban.jianzcar@outlook.com>
2025-06-10 14:28:25 +0800

    fix imports

--- src/includes/imports.php
+++ src/includes/imports.php
@@ -2,5 +2,4 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/alpinejs/dist/cdn.min.js" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.4/dist/htmx.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/d3/dist/d3.min.js"></script>
 <?php include_once(__DIR__ . '/esbuild.php'); ?>
